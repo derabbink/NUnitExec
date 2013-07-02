a7be611a09f93b5c49dcc642a59635f50e50efb1
Maarten Abbink <m.abbink@fugro.nl>
2013-07-02 10:38:08 +0200

    started with some basic code

---
+++ SomeLib.Test/OperationsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SomeLib.Test
+{
+    [TestFixture]
+    public class OperationsTest
+    {
+        [Test]
+        public void Increase5()
+        {
+            int inp = 5;
+            int expected = 5 + 1;
+            int actual = Operations.Increase(inp);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ SomeLib/Operations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomeLib
+{
+    public class Operations
+    {
+        public static int Increase(int number)
+        {
+            return ++number;
+        }
+    }
+}
