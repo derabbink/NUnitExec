37d44d19f973760c6a040ed905b827743fcde74c
Maarten Abbink <m.abbink@fugro.nl>
2013-07-02 11:42:45 +0200

    added simple test execution

--- NUnitWrapper/Program.cs
+++ NUnitWrapper/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using NUnit.Core;
+using NUnit.Core.Filters;
 
 namespace NUnitWrapper
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
+            CoreExtensions.Host.InitializeService();
+            SimpleTestRunner runner = new SimpleTestRunner();
+            TestPackage package = new TestPackage("CodeTests");
+            package.Assemblies.Add("..\\..\\..\\SomeLib.Test\\bin\\Debug\\SomeLib.Test.dll");
+            if (runner.Load(package))
+            {
+                TestResult result = runner.Run(new NullListener(), TestFilter.Empty, true, LoggingThreshold.All);
+            }
+
+            Console.ReadLine();
         }
     }
 }
